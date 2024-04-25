46bd2529a9b103ec3f6c7aa1245be6ed58ff0c01
nadimmorol890 <nadimmorol890@gmail.com>
2024-04-25 13:06:32 +0600

    add Binary Search - 1

---
+++ Class/Binary Search - 1/Binary_Search.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n, key;
+    cin >> n >> key;
+    vector<int> a(n);
+    for (int i = 0;i < n;i++) {
+        cin >> a[i];
+    }
+
+    int l = 0, r = n - 1, mid, idx = -1;
+    while (l <= r) {
+        mid = (l + r) / 2;
+        if (key == a[mid]) {
+            idx = mid;
+            break;
+        }
+        else if (key < a[mid]) {
+            r = mid - 1;
+        }
+        else {
+            l = mid + 1;
+        }
+    }
+
+    if (idx == -1) {
+        cout << "Element not found" << '\n';
+    }
+    else {
+        cout << "Element found at index " << idx << '\n';
+    }
+
+    return 0;
+}
---
+++ Class/Binary Search - 1/Last_Occurrence.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n, key;
+    cin >> n >> key;
+    vector<int> a(n);
+    for (int i = 0;i < n;i++) {
+        cin >> a[i];
+    }
+
+    int l = 0, r = n - 1, mid, idx = -1;
+    while (l <= r) {
+        mid = (l + r) / 2;
+        if (key == a[mid]) {
+            idx = mid;
+            l = mid + 1;
+        }
+        else if (key < a[mid]) {
+            r = mid - 1;
+        }
+        else {
+            l = mid + 1;
+        }
+    }
+
+    cout << idx << '\n';
+
+    return 0;
+}
